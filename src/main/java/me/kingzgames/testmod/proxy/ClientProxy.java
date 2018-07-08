ab28771f8fdd046fc24575a1640f758bd2da51b4
KingdevNL <jasper@kingdev.nl>
2018-07-08 10:38:45 +0200

    Fixes

--- src/main/java/me/kingzgames/testmod/proxy/ClientProxy.java
+++ src/main/java/me/kingzgames/testmod/proxy/ClientProxy.java
@@ -4,20 +4,17 @@ import me.kingzgames.testmod.init.ModBlocks;
 import me.kingzgames.testmod.init.ModItems;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod.EventBusSubscriber
+
 public class ClientProxy extends CommonProxy {
 
     @Override
     public void onPreInit(FMLPreInitializationEvent e) {
         super.onPreInit(e);
-        ModItems.registerRenders();
-        ModBlocks.registerRenders();
         MinecraftForge.EVENT_BUS.register(this);
     }
 
@@ -34,6 +31,8 @@ public class ClientProxy extends CommonProxy {
     @SubscribeEvent
     public void onModelReg(ModelRegistryEvent e) {
         System.out.println("ClientProxy.onModelReg");
+        ModItems.registerRenders();
+        ModBlocks.registerRenders();
     }
 
 
